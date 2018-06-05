142f6774ac96f205458c0d900cc66bd518f81f5a
Taro Tokyo <ec2-user@ip-172-31-87-30.ec2.internal>
2018-06-05 03:20:06 +0000

    add column

---
+++ database/migrations/2018_06_05_104407_drop_status_in_tasks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropStatusInTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+            //
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+}
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,7 +7,7 @@
 
 
     @foreach ($tasks as $task)
-                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
+                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->status }} >{{ $task->content }}</li>
             @endforeach
             
     {!! link_to_route('tasks.create', '新規タスクの投稿') !!}
